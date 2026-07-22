8fc0df261beaf13c9bf17723938c023484014333
Joel Varghese Jose <joelvjoseadoor@gmail.com>
2026-07-22 03:24:56 +0000

    Wire sections 5-9 into Home (Phase 3 complete)

--- strive-website/src/pages/Home.jsx
+++ strive-website/src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import Hero from '../sections/Hero'
 import TrustMetrics from '../sections/TrustMetrics'
 import About from '../sections/About'
+import WhyChooseStrive from '../sections/WhyChooseStrive'
+import WhatWeDo from '../sections/WhatWeDo'
+import OurApproach from '../sections/OurApproach'
+import MembershipBenefits from '../sections/MembershipBenefits'
+import MembershipProcess from '../sections/MembershipProcess'
 
 export default function Home() {
   return (
@@ -8,6 +13,11 @@ export default function Home() {
       <Hero />
       <TrustMetrics />
       <About />
+      <WhyChooseStrive />
+      <WhatWeDo />
+      <OurApproach />
+      <MembershipBenefits />
+      <MembershipProcess />
     </>
   )
 }
